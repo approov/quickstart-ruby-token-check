171d3ce36942d2ba701db96de7cd6d7dd0a502bd
Exadra37 <exadra37@gmail.com>
2020-09-16 15:14:45 +0100

    Initial pass to implement the Approov Quickstart without using a framework.
    * Hello World servers:
        + Unprotected server example is the reference implementation for the Approov protected servers.
        + Approov token protected server example built from the unprotected server.
        + Approov token binding protected server example built from the Approov token protected server.
    * Docs:
        + Approov quickstarts to implement the Approov token check and the Approov token binding check integrations.
        + How to run and test the Approov integration examples that are the base for the Approov quickstarts.
    
    Signed-off-by: Exadra37 <exadra37@gmail.com>

---
+++ src/approov-protected-server/token-binding-check/hello-server-protected.rb
@@ -0,0 +1,141 @@
+require 'socket'
+require 'json'
+require 'dotenv'
+require 'jwt'
+require 'base64'
+require 'digest'
+
+env = Dotenv.parse(".env")
+
+if not env['HTTP_PORT']
+    env['HTTP_PORTP'] = 8002
+end
+
+if not env['SERVER_HOSTNAME']
+    env['SERVER_HOSTNAME'] = '127.0.0.1'
+end
+
+if not env['APPROOV_BASE64_SECRET']
+    raise "Missing in the .env file the value for the variable: APPROOV_BASE64_SECRET"
+end
+
+APPROOV_SECRET = Base64.decode64(env['APPROOV_BASE64_SECRET'])
+
+def verifyApproovToken headers
+    begin
+        approov_token = headers['approov-token']
+
+        if not approov_token
+            # You may want to add some logging here
+            return nil
+        end
+
+        options = { algorithm: 'HS256' }
+        approov_token_claims, header = JWT.decode approov_token, APPROOV_SECRET, true, options
+
+        return approov_token_claims
+    rescue JWT::DecodeError => e
+        # You may want to add some logging here
+    rescue JWT::ExpiredSignature => e
+        # You may want to add some logging here
+    rescue JWT::InvalidIssuerError => e
+        # You may want to add some logging here
+    rescue JWT::InvalidIatError
+        # You may want to add some logging here
+    end
+end
+
+def verifyApproovTokenBinding headers, approov_token_claims
+
+    # Note that the `pay` claim will, under normal circumstances, be present,
+    # but if the Approov failover system is enabled, then no claim will be
+    # present, and in this case you want to return true, otherwise you will not
+    # be able to benefit from the redundancy afforded by the failover system.
+    if not approov_token_claims['pay']
+        # You may want to add some logging here
+        return true
+    end
+
+    # We use the Authorization token, but feel free to use another header in
+    # the request. Beqar in mind that it needs to be the same header used in the
+    # mobile app to qbind the request with the Approov token.
+    token_binding_header = headers['authorization']
+
+    if not token_binding_header
+        # You may want to add some logging here
+        return false
+    end
+
+    # We need to hash and base64 encode the token binding header, because that's
+    # how it was included in the Approov token on the mobile app.
+    token_binding_header_encoded = Digest::SHA256.base64digest token_binding_header
+
+    if not approov_token_claims['pay'] === token_binding_header_encoded
+        # You may want to add some logging here
+        return false
+    end
+
+    return true
+end
+
+def requestHeaders connection
+    headers = {}
+
+    while line = connection.gets
+        case line
+        when "\r\n"
+            break
+        else
+            key, value = line.split(': ')
+            headers[key.downcase] = value.strip
+        end
+    end
+
+    headers
+end
+
+def requestPath connection
+    first_line = connection.gets
+    puts first_line
+
+    request_verb, full_path = first_line.split(' ')
+    path = full_path.split('?')
+    path[0]
+end
+
+def sendResponse connection, http_status_code, response_body
+    connection.write "HTTP/1.1 #{http_status_code}\r\n"
+    connection.write "Content-Type: application/json\r\n"
+    connection.write "Content-Length: #{response_body.length}\r\n"
+    connection.write "\r\n"
+    connection.write response_body
+    connection.close
+end
+
+server = TCPServer.new env['SERVER_HOSTNAME'], env['HTTP_PORT']
+
+while connection = server.accept
+    path = requestPath(connection)
+    headers = requestHeaders(connection)
+
+    if not approov_token_claims = verifyApproovToken(headers)
+        sendResponse(connection, 401, JSON.generate({}))
+        next
+    end
+
+    if not verifyApproovTokenBinding(headers, approov_token_claims)
+        sendResponse(connection, 401, JSON.generate({}))
+        next
+    end
+
+    case path
+    when "/"
+        http_status_code = 200
+        response_body = JSON.generate({:message => "Hello, World!"})
+    else
+        http_status_code = 401
+        response_body = JSON.generate({})
+    end
+
+    sendResponse(connection, http_status_code, response_body)
+end
---
+++ src/unprotected-server/README.md
@@ -0,0 +1,71 @@
+# Unprotected Server Example
+
+The unprotected example is the base reference to build the [Approov protected servers](/src/approov-protected-server/). This a very basic Hello World server.
+
+
+## TOC - Table of Contents
+
+* [Why?](#why)
+* [How it Works?](#how-it-works)
+* [Requirements](#requirements)
+* [Try It](#try-it)
+
+
+## Why?
+
+To be the starting building block for the [Approov protected servers](/src/approov-protected-server/), that will show you how to lock down your API server to your mobile app. Please read the brief summary in the [README](/README.md#why) at the root of this repo or visit our [website](https://approov.io/product.html) for more details.
+
+[TOC](#toc---table-of-contents)
+
+
+## How it works?
+
+The Ruby server is very simple and is defined in the file [src/unprotected-server/hello-server-unprotected.rb](/src/unprotected-server/hello-server-unprotected.rb).
+
+The server only replies to the endpoint `/` with the message:
+
+```json
+{"message": "Hello, World!"}
+```
+
+[TOC](#toc---table-of-contents)
+
+
+## Requirements
+
+To run this example you will need to have Ruby installed. If you don't have then please follow the official installation instructions from [here](https://www.ruby-lang.org/en/documentation/installation/) to download and install it.
+
+[TOC](#toc---table-of-contents)
+
+
+## Try It
+
+First, install the `dotenv` package to read the `.env` file.
+
+```text
+gem install dotenv
+```
+
+Now, run this example from the `src/unprotected-server` folder with:
+
+```text
+ruby hello-server-unprotected.rb
+```
+
+Finally, you can test that it works with:
+
+```text
+curl -iX GET 'http://localhost:8002'
+```
+
+The response will be:
+
+```text
+HTTP/1.1 200
+Content-Type: application/json
+Content-Length: 27
+
+{"message":"Hello, World!"}
+```
+
+[TOC](#toc---table-of-contents)
